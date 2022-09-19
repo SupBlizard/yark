42ad88a0e3f6f331d048fdcf7cde369676de6cbb
SupBlizard <58865670+SupBlizard@users.noreply.github.com>
2022-09-19 01:50:51 +0100

    Update Logger & add an error formating function

--- utils.py
+++ utils.py
@@ -1,21 +1,25 @@
-import colorama
+from colorama import *
 
 # CONSTANTS
 DEFAULT_DESC = "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube."
 
+def err_format(msg, id=""):
+    if id: id = f"[youtube] {id}: "
+    return f"{Fore.RED}ERROR: {Style.RESET_ALL}{id}{msg}"
 
 # Custom logger
 class Logger(object):
     def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        # Save event in debug log
-        pass
-
-    def error(self, msg):
-        # Save event in debug log
         print(msg)
 
-    def info(msg, vid):
-        print(f"{colorama.Fore.CYAN}INFO:{colorama.Style.RESET_ALL}  [youtube] {vid}: {msg}")
+    def warning(self, msg):
+        # TODO: Save event in debug log
+        pass
+
+    def error(self, msg=None):
+        # TODO: Save event in debug log
+        if msg: print(msg)
+
+    def info(msg, vid=""):
+        vid = "" if vid=="" else " "+vid
+        print(f"{Fore.CYAN}INFO: {Style.RESET_ALL} [youtube]{vid}: {msg}")
